7ddd9bed83870b282ed57814aa916758c4767ac3
theo <teddyhoover@gmail.com>
2021-02-01 11:25:52 -0600

    intial

---
+++ lib/strong/grips.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "grips/version"
+
+module Strong
+  module Grips
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/strong/grips/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Strong
+  module Grips
+    VERSION = "0.1.0"
+  end
+end
