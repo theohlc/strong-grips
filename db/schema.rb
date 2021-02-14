96165c6138494cdcff9735983d6157adfcb97c9d
theo <teddyhoover@gmail.com>
2021-02-14 16:03:31 -0600

    removed example data

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_221717) do
-
-  create_table "hangs", force: :cascade do |t|
-    t.string "exercise"
-    t.integer "duration"
-    t.integer "user_id"
-    t.datetime "time"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
